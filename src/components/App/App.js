32a8e8349be7eaf439b315be8007e7a1ed5ef99b
Patrick Becker <patrickgbecker@gmail.com>
2022-09-07 10:42:32 -0500

    Add trix, form, and card files with fetch function in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,30 @@
 import { Component } from 'react';
+import getTrix from '../../apiCalls';
+import Trix from './Trix';
+import Form from './Form';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      trix: [],
+      error: ''
+    }
+  }
+
+  getAllTrix = () => {
+    getTrix.fetchTrixData('http://localhost:3001/api/v1/tricks')
+      .then(data => {
+        console.log("trix data: ", data)
+        this.setState({trix: data})
+      })
+      .catch(error => {
+        console.log(error)
+        this.setState({error: `${error}`})
+      })
+  }
+
   render() {
     return (
       <div className="App">
